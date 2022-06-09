69ef2ac059e32dfc9a2e55e3bc7a642eb09ab573
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-06-09 14:01:43 -0300

    Makefile target to run all tests.
    Adds a Makefile target to run all the tests available in the E2E test
    repository.

--- scripts/generate_resources_dir.sh
+++ scripts/generate_resources_dir.sh
@@ -8,7 +8,7 @@ mkdir $RESOURCES_DIR/resources_$CRD_VERSION
 
 find $RESOURCES_DIR -maxdepth 1 -type f  -exec cp \{\}  $RESOURCES_DIR/resources_$CRD_VERSION \;
 
-kubewarden_resources_files=$(grep -rl "apiVersion: policies.kubewarden.io" $RESOURCES_DIR/resouces_$CRD_VERSION)
+kubewarden_resources_files=$(grep -rl "apiVersion: policies.kubewarden.io" $RESOURCES_DIR/resources_$CRD_VERSION)
 for file in $kubewarden_resources_files 
 do 
 	yq --in-place -Y ".apiVersion =\"policies.kubewarden.io/$CRD_VERSION\"" $file
